bb3c68584e48fb0352d97c5a125c3e6bfff72c99
kojix2 <2xijok@gmail.com>
2020-10-17 23:28:26 +0900

    Add Axis methods mechanically

--- lib/gr/plot/axes.rb
+++ lib/gr/plot/axes.rb
@@ -5,6 +5,45 @@ module GR
     class Axes
       attr_accessor :kind, :ranges, :tickdata, :ticklabels, :perspective, :camera, :options
       def initialize; end
+
+      # Calculation of data ranges
+      def minmax; end
+
+      def extrema64; end
+
+      def fix_minmax; end
+
+      def adjustranges!; end
+
+      def set_ticks; end
+
+      def set_axis; end
+
+      def set_ticklabels!; end
+
+      def ticklabel_fun; end
+
+      def set_scale; end
+
+      def _rotate!; end
+
+      def _tilt; end
+
+      def _focus!; end
+
+      def set_camera; end
+
+      def graham_hull; end
+
+      def axes3frame; end
+
+      def fillaxesbackground; end
+
+      def draw; end
+
+      def draw_polaraxes; end
+
+      def _tickcharheight; end
     end
   end
 end
