a4ac20e4dd105cc7c6561306ece99678fb021514
kojix2 <2xijok@gmail.com>
2020-10-18 00:20:18 +0900

    Add Geometry methods mechanically

--- lib/gr/plot/geometry.rb
+++ lib/gr/plot/geometry.rb
@@ -2,11 +2,15 @@
 
 module GR
   class Plot
-    class Geometries
+    class Geometry
       attr_accessor :kind, :x, :y, :z, :c, :spec, :label, :attributes
       def initialize; end
 
+      def _uselinespec; end
+
       def draw; end
+
+      def normalize_color; end
     end
   end
 end
